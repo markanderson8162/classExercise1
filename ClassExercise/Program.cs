79207fa4df5ae81a00f6400dd7c5e7e3dc54d4fd
Mark Anderson <markanderson8162@gmail.com>
2021-06-18 11:54:08 -0500

    finished first class exercise

---
+++ ClassExercise/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ClassExercise
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var car1 = new Car();
+			car1.Make = "GMC";
+			car1.Model = "Sierra";
+			car1.Year = 2021;
+
+			Console.WriteLine($"My new truck is a {car1.Year} {car1.Make} {car1.Model}.");
+
+		}
+
+
+	}
+}
